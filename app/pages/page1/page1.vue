d1bb3ad42c37debf44589086f8d57c552ca9cc47
DBZiou <3224627721@qq.com>
2025-09-11 14:21:43 +0800

    feat: 完成前端 webpack 工程化搭建

---
+++ app/pages/page1/page1.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>page1</h1>
+  <input
+    v-model="content" 
+    type="text" 
+    placeholder="请输入内容"
+  >
+  <div>{{ content }}</div>
+  <div>{{ name }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const content = ref('')
+const name = ref('xiaowu')
+console.log('page1 init')
+</script>
+
+<style lang="less" scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ app/pages/page2/page2.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>page2</h1>
+  <input
+    v-model="content"
+    type="text"
+    placeholder="请输入内容"
+  >
+  <div>{{ content }}</div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const content = ref('')
+  console.log('page2 init')
+</script>
+
+<style lang="less" scoped>
+h1{
+  color: green;
+}
+</style>
